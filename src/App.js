6ab795da65a4ec397f4237d376918d6ff7e1f9fe
Sophia de Albuquerque Eleuterio <2302129@SALAS.AULAS>
2025-03-13 22:17:11 -0300

    atualização 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Home from './componentes/home/home';
 import Logo from "./componentes/logo/index"
 import user from "./imgs/user.png"
 
-const navbarOpcoes = ['Crie a sua conta', 'Entrar', 'Compras']
+const navbarOpcoes = ['Eventos', 'Esportes', 'Sobre']
 const navbarIcones = [user]
 
 function App() {
@@ -18,10 +19,15 @@ function App() {
 
         <ul className='icones'>
           {navbarIcones.map((icone) => (
-            <li className='icone'><img src={icone} width="20" height="20"></img></li>
+            <li className='icone'><img src={icone} width="50" height="50"></img></li>
           ))}
         </ul>
       </header>
+      <main>
+        <ul className='home'>
+          <Home></Home>
+        </ul>
+      </main>
     </div>
   );
 }
